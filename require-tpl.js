352a099d1718b91ff96a589b38379e5419503439
Matt DeClaire <matt@declaire.com>
2014-02-17 14:20:27 -0800

    optimizer

--- require-tpl.js
+++ require-tpl.js
@@ -1,8 +1,27 @@
-define(['jquery', 'text'], function($, text) {
-	return $.extend({}, text, {
-		load: function(name, parentRequire, onload, config) {
-			var filename = '/templates/' + name + '.tpl';
-			text.load(filename, parentRequire, onload, config);
+define(['text'], function(text) {
+	var buildMap = {};
+
+	return {
+		load: function(name, req, onLoad, config) {
+			if (config.isBuild && config.inlineTpl === false) return onLoad();
+
+			var load = function(data) {
+				if (config.isBuild) buildMap[name] = data;
+				onLoad(data);
+			};
+
+			text.get(req.toUrl('templates/' + name + '.tpl'), load, onLoad.error);
+		},
+
+		write: function (pluginName, moduleName, write, config) {
+			if (buildMap.hasOwnProperty(moduleName)) {
+				var content = text.jsEscape(buildMap[moduleName]);
+
+				write.asModule(
+					pluginName + "!" + moduleName,
+					"define(function () { return '" + content + "'; });\n"
+				);
+			}
 		}
-	});
+	};
 });
